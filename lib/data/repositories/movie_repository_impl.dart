bc534699fbc93f9b81d7d0c8513b7977ea3eb4a2
mo15hammed <mo15hammed@hotmail.com>
2021-10-21 17:05:36 +0200

    repositories and use cases.

---
+++ lib/data/repositories/movie_repository_impl.dart
@@ -0,0 +1,52 @@
+import 'package:movies_app/data/data_sources/movie_remote_data_source.dart';
+import 'package:movies_app/data/models/movie_model.dart';
+import 'package:movies_app/domain/entities/app_error.dart';
+import 'package:movies_app/domain/entities/movie_entity.dart';
+import 'package:movies_app/domain/repositories/movie_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class MovieRepositoryImpl extends MovieRepository {
+  final MovieRemoteDataSource remoteDataSource;
+
+  MovieRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<AppError, List<MovieModel>>> getTrending() async {
+    try {
+      final movies = await remoteDataSource.getTrending();
+      return Right(movies);
+    } on Exception {
+      return Left(AppError('Something went wrong!'));
+    }
+  }
+
+  @override
+  Future<Either<AppError, List<MovieEntity>>> getPopular() async {
+    try {
+      final movies = await remoteDataSource.getPopular();
+      return Right(movies);
+    } on Exception {
+      return Left(AppError('something went wrong!'));
+    }
+  }
+
+  @override
+  Future<Either<AppError, List<MovieEntity>>> getPlayingNow() async {
+    try {
+      final movies = await remoteDataSource.getPlayingNow();
+      return Right(movies);
+    } on Exception {
+      return Left(AppError('something went wrong!'));
+    }
+  }
+
+  @override
+  Future<Either<AppError, List<MovieEntity>>> getComingSoon() async {
+    try {
+      final movies = await remoteDataSource.getComingSoon();
+      return Right(movies);
+    } on Exception {
+      return Left(AppError('something went wrong!'));
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:movies_app/data/data_sources/movie_remote_data_source.dart';
-
+import 'package:movies_app/data/repositories/movie_repository_impl.dart';
+import 'package:movies_app/domain/entities/movie_entity.dart';
+import 'package:movies_app/domain/entities/no_params.dart';
+import 'package:movies_app/domain/repositories/movie_repository.dart';
+import 'package:movies_app/domain/usecases/get_trending.dart';
+import 'package:dartz/dartz.dart';
 import 'data/core/dio_helper.dart';
+import 'domain/entities/app_error.dart';
 
 Future<void> main() async {
   DioHelper dioHelper = DioHelper(Dio());
   MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(dioHelper);
+  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
+  GetTrending getTrending = GetTrending(movieRepository);
 
-  dataSource.getTrending();
-  dataSource.getPopular();
-  dataSource.getPlayingNow();
-  dataSource.getComingSoon();
+  final Either<AppError, List<MovieEntity>> eitherRes = await getTrending(NoParams());
+
+  eitherRes.fold((l) {
+    print('error');
+    print(l.message);
+  }, (movies) {
+    print('list of movies');
+    print(movies);
+  });
 
   runApp(MovieApp());
 }
@@ -24,4 +37,3 @@ class MovieApp extends StatelessWidget {
     return Container();
   }
 }
-
